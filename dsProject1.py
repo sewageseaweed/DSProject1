6237c0c1f9bcede0503354f55ff25937d8879524
Daniel Rangel <drangelfigueroa@csumb.edu>
2021-03-20 16:38:02 -0700

    Added "Distraction Type in Injury Severity" and "Distraction in Injury Severity" plots

--- dsProject1.py
+++ dsProject1.py
@@ -7,8 +7,14 @@ Created on Thu Mar 18 14:58:59 2021
 
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
+# clyde
 df = pd.read_csv(r"C:\Users\Clyde\Desktop\Crash_Reporting_-_Drivers_Data.csv")
+
+# daniel
+# df =  pd.read_csv("/home/daniel/Documents/cst383/project/Crash_Reporting_-_Drivers_Data.csv")
 
 print(df.columns)
 df['Driver Distracted By'].value_counts()
@@ -24,5 +30,43 @@ df['Non-Motorist Substance Abuse'].value_counts()
 #Severity injury with distraction, equip problems with both.
 #Vehicle year, make, model, with injury severity/equipment problems
 
+#
 
-df[]
+############# PLOT 1 #############
+# CHECKS IF DISTRACTED 
+def distracted(x):
+    if x == 'NOT DISTRACTED':
+        return False
+    return True
+# NEW COLUMN IN df
+df['DISTRACTED'] = df['Driver Distracted By'].apply(distracted)
+
+# PLOT 
+plt.figure(figsize=(30,15))
+for i in enumerate(df['Injury Severity'].unique()):
+    plt.subplot(1,5, i[0]+1)
+    plt.title(i[1])
+    sns.countplot(x='DISTRACTED', data= df[ df['Injury Severity'] == i[1] ])
+    plt.rc('xtick',labelsize=8)
+    plt.ylabel("Events")
+plt.suptitle("Distraction in Injury Severity", fontsize=20, weight = 'bold')
+
+############# PLOT 2 #############
+
+# Cleans 'Driver Distracted By'
+def phone(x):
+    if 'CELL PHONE' in x:
+        return 'CELL PHONE'
+    return x
+df['Driver Distracted By'] = df['Driver Distracted By'].apply(phone)
+
+# PLOT
+plt.figure(figsize=(15,20))
+for i in enumerate(df['Injury Severity'].unique()):
+    plt.subplot(5,1, i[0]+1)
+    sns.countplot(y='Driver Distracted By', data= df[ (df['Injury Severity'] == i[1]) & (df['Driver Distracted By'] != 'NOT DISTRACTED') ])
+    plt.rc('xtick',labelsize=8)
+    plt.xlabel("")
+    plt.ylabel("Distraction")
+    plt.title(i[1])
+plt.suptitle("Distraction Type in Injury Severity", fontsize=15, weight = 'bold')
